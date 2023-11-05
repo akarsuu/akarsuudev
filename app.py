12c25b48b42ecc0dd1a3532ec6a113ab8153b34f
akarsuu <103823031+akarsuu@users.noreply.github.com>
2023-11-05 14:06:56 +0300

    Add files via upload
    like counts revised

--- app.py
+++ app.py
@@ -20,35 +20,35 @@ class Likes(db.Model):
     totalLike = db.Column(db.Integer, default=0)
 
 
+
 with app.app_context():
     db.create_all()
 
 @app.route('/like', methods=['POST'])
 def add_like():
-    ip = request.remote_addr
-    
-
+    ip = request.headers.get('X-Forwarded-For')  # Get client's IP address from the header
     now = datetime.now()
-    
+
     # Check if the IP has already liked the specific content
     existing_like = Likes.query.filter_by(ip=ip).first()
+
     if existing_like:
-        return jsonify({'message': "Like already counted", 'totalLike':total_likes})
-  
+        # IP has already liked, so no action needed
+        total_likes = Likes.query.with_entities(func.sum(Likes.likeCount)).scalar()
+        return jsonify({'message': "Like already counted", 'totalLike': total_likes or 0})
+
+    # Calculate the totalLikes for the specific content
+    total_likes = Likes.query.with_entities(func.sum(Likes.likeCount)).scalar() or 0
 
     # Create a new like record for the specific content
-    like = Likes(ip=ip, date_created=now, likeCount=1)
-    
+    likes = Likes(ip=ip, date_created=now, likeCount=1, totalLike=total_likes + 1)
+
     # Add the like record to the database
-    db.session.add(like)
+    db.session.add(likes)
     db.session.commit()
 
-    # Update the totalLikes for the specific content
-    total_likes = Likes.query.with_entities(func.sum(Likes.likeCount)).scalar()
-
     message = "Thank you for your support :)"
-
-    return jsonify({'totalLike': total_likes, "message": message})
+    return jsonify({'totalLike': total_likes + 1, "message": message})
 
 @app.route('/')
 def index():
@@ -57,3 +57,7 @@ def index():
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=8000, debug=True)
+
+
+
+   
